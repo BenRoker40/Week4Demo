f35455ea5d0b2d240f3c06d392f4e6d0f41c645c
roker <roker@DESKTOP-TJV53Q5.lan>
2022-09-26 18:16:00 -0600

    Converted application into a full MVC pattern.

--- src/java/servlets/HelloWorldServlet.java
+++ src/java/servlets/HelloWorldServlet.java
@@ -1,15 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package servlets;
-
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import models.Person;
 
 /**
  *
@@ -31,16 +26,17 @@ public class HelloWorldServlet extends HttpServlet {
         String firstname = request.getParameter("firstname");
         String lastname = request.getParameter("lastname");
         
+        if (firstname == null || firstname.equals("") || lastname == null || lastname.equals("")) {
+        request.setAttribute("message", "Invalid entry. Please enter both your first and last name.");
         request.setAttribute("firstname", firstname);
         request.setAttribute("lastname", lastname);
-        
-        if (firstname == null || firstname.equals("") || lastname == null || lastname.equals("")) {
-            request.setAttribute("message", "Invalid entry. Please enter both your first and last name.");
-            
             getServletContext().getRequestDispatcher("/WEB-INF/helloWorldForm.jsp")
                 .forward(request, response);
             return;
         }
+        
+        Person person = new Person(firstname, lastname);
+        request.setAttribute("person", person);
         
         getServletContext().getRequestDispatcher("/WEB-INF/sayHello.jsp")
                 .forward(request, response);
